3522a7278879980801b9334d9de66cee1ef480d3
owen <owenshaw90d@gmail.com>
2025-02-27 13:50:44 +0000

    Commiting first itteration of notes app

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Notes;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		Routing.RegisterRoute(nameof(Views.NotePage), typeof(Views.NotePage));
+
+		MainPage = new AppShell();
+	}
+}
